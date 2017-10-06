8279bf97370b9a2e8172f0d6e212262ebc21f6cf
katelyn <katelyn.e.lemay@gmail.com>
2017-10-06 13:50:48 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+/*enable toggle for nav*/
+$(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
 
+/*make navbar-toggler white on scroll
+ */
+function checkScroll(){
+    var startY = $('.header').height(); 
+
+    if($(window).scrollTop() > startY){
+        $('.navbar-toggler').addClass("scrolled");
+       
+    }else{
+        $('.navbar-toggler').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+
+}
+  
